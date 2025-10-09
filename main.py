e2f1581565c14ee98fce99d1223a94ac5e43fbed
FanRobots <fanrobots@gmail.com>
2025-10-09 12:57:32 +0300

    database security fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-from docutils.nodes import table
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -15,50 +14,55 @@ class GraveyardsScreen(Screen):
 
 class DataBase:
     def __init__(self):
-        self.connection = sqlite3.connect("graveyards.db")
-        self.cursor = self.connection.cursor()
+        self._connection = sqlite3.connect("graveyards.db")
+        self._cursor = self._connection.cursor()
         self.create_tables_names_tables()
 
     def create_tables_names_tables(self):
-        self.cursor.execute("""
+        self._cursor.execute("""
         CREATE TABLE IF NOT EXISTS names_tables (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             name_in_system TEXT NOT NULL)
         """)
-        self.connection.commit()
+        self._connection.commit()
 
     def create_table_graveyards(self, table, name_table):
-        self.cursor.execute(f"""
+        self._cursor.execute(f"""
         CREATE TABLE IF NOT EXISTS {table} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             content TEXT NOT NULL )
         """)
-        self.cursor.execute("INSERT INTO names_tables(name, name_in_system) VALUES (?,?)",(name_table, table))
-        self.connection.commit()
+        self._cursor.execute("INSERT INTO names_tables(name, name_in_system) VALUES (?,?)",(name_table, table))
+        self._connection.commit()
 
     def add_new_gravestone_in_table(self, table, name_gravestone, content):
-        self.cursor.execute(f"INSERT INTO {table}(name, content) VALUES (?,?)",(name_gravestone, content))
-        self.connection.commit()
+        self._cursor.execute(f"INSERT INTO {table}(name, content) VALUES (?,?)",(name_gravestone, content))
+        self._connection.commit()
 
     def get_name_table(self, table, row_id):
-        self.cursor.execute(f"SELECT name, name_in_system FROM {table} WHERE id=(?)", row_id)
-        return self.cursor.fetchone()
+        self._cursor.execute(f"SELECT name, name_in_system FROM {table} WHERE id=(?)", row_id)
+        return self._cursor.fetchone()
 
     def get_row_from_table(self, table, row_id):
-        self.cursor.execute(f"SELECT id, name, content FROM {table} WHERE id=(?)", row_id)
-        return self.cursor.fetchone()
+        self._cursor.execute(f"SELECT id, name, content FROM {table} WHERE id=(?)", row_id)
+        return self._cursor.fetchone()
 
     def close(self):
-        self.connection.close()
+        self._connection.close()
 
 
 class GraveyardOfMind(App):
+    def __init__(self, **kwargs):
+        self.data_base = DataBase()
+        super().__init__(**kwargs)
+
     def build(self):
         Window.size = (365, 650)
-        Builder.load_file("style.kv")
         return Builder.load_file("screens.kv")
 
 if __name__ == '__main__':
-    GraveyardOfMind().run()
+    app = GraveyardOfMind()
+    app.run()
+    app.data_base.close()
